9993cb3a168bbc39bd9802f3be359cc52d9c9e85
hecmandark <{email}>
2025-04-26 22:20:08 +0200

    Add dynamic allowed hosts configuration in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,18 @@ SECRET_KEY = 'django-insecure-s*=*^wvygth=n&+pyww+j81y=f8z9s9z)s%_kc)#ihjgw!d)m5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "portfolio-mag9.onrender.com", 
+    "localhost",  
+    "127.0.0.1",  
+]
+
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(
+        RENDER_EXTERNAL_HOSTNAME
+    )  
 
 
 # Application definition
