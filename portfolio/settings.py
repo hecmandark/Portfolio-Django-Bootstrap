97b4e0ecea8a0e68c53f131e42df36383adb8100
hecmandark <{email}>
2025-04-28 19:00:36 +0200

    Refactor DATABASES setting to use os.getenv for improved readability

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -92,8 +92,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.getenv("DATABASE_URL"), 
         conn_max_age=600,
+        
     )
 }
 
