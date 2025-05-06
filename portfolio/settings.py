5a0e177ebfac31f7f8cf237a27a313ad5f1e9ed3
hecmandark <{email}>
2025-05-06 13:20:43 +0200

    Add Whitenoise static file storage configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -155,3 +155,9 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
